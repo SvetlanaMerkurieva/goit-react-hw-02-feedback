2d336f9e0000c71278b5ee1d9be22bf463af601f
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-12-05 22:44:57 +0300

    Add class-component Feedbacks (step 1, 2)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
-import s from "./App.module.css";
+import { Feedbacks } from '../Feedbacks/Feedbacks';
+import s from './App.module.css';
 
 function App() {
   return (
     <div className={s.app}>
-      <header className={s.appHeader}></header>
+      <header className={s.appHeader}>
+        <Feedbacks />
+      </header>
     </div>
   );
 }
