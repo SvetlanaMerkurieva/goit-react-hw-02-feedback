3ad94e3cfc01c93183a17c0e452e894f11a97ed2
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-12-05 20:29:02 +0300

    Corrects original files React

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# goit-react-hw-02-feedback
-Домашнее задание React № 2
---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import s from "./App.module.css";
+
+function App() {
+  return (
+    <div className={s.app}>
+      <header className={s.appHeader}></header>
+    </div>
+  );
+}
+
+export default App;
